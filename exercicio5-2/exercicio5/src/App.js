cf8f6391a20cea8ac613c8fb57a776743d9171e5
fbhkim <RM87067@fiap.com.br>
2021-09-23 12:34:50 -0300

    import rodape para app

--- exercicio5-2/exercicio5/src/App.js
+++ exercicio5-2/exercicio5/src/App.js
@@ -1,11 +1,13 @@
 import Cabecalho from './components/cabecalho/Cabecalho.jsx'
 import Corpo from './components/corpo/Corpo.jsx'
+import Rodape from './components/rodape/Rodape.jsx'
 
 function App() {
   return (
     <>
     <Cabecalho/>
     <Corpo/>
+    <Rodape/>
    </>
   );
 }
