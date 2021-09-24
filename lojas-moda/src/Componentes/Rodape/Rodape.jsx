c75dc9aa956c58f470ccc73f2accb849c7782135
Matheus Cruz <mccruz2611@gmail.com>
2021-09-24 09:25:48 -0300

    termino do html no rodape e criacao do style components

---
+++ lojas-moda/src/Componentes/Rodape/Rodape.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {FlexRodape, Direitos} from '../style.js'
+
+export default function Rodape() {
+    return (
+        <FlexRodape>
+            <Direitos>ZenModas 2021 - Todos os direitos reservados.</Direitos>
+        </FlexRodape>
+    )
+}
