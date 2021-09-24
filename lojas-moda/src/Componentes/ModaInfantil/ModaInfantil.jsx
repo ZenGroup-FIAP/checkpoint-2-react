03d7be1e9d43a18a5b575eeee44c654391cd8247
luisduccini <rm88360@fiap.com.br>
2021-09-24 16:52:24 -0300

    alterando infantil

--- lojas-moda/src/Componentes/ModaInfantil/ModaInfantil.jsx
+++ lojas-moda/src/Componentes/ModaInfantil/ModaInfantil.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import Camisetas from "../../img/infantil/camisetas-infantil.jpg"
+import Calcas from "../../img/infantil/calcas-infantil.jpg"
+import Calcados from "../../img/infantil/calcas-infantil.jpg"
+import Variados from "../../img/infantil/variados-infantil.jpg"
+
 
 export default function ModaInfantil() {
     return (
         <div>
-            <h1> Moda Infantil </h1>
-            <p>O mais confortável, para quem mais amamos.  </p>
+            <h1>Moda Infatil</h1>
+            <p>O mais corfortável, para quem mais amamos.</p>
+
+            <ul>
+                <li>
+                    <img src={Camisetas} alt="Camisetas em cabides" />
+                    <p>Camisetas</p>
+                </li>
+                <li>
+                    <img src={Calcas} alt="Calça preta" />
+                    <p>Calças</p>
+                </li>
+                <li>
+                    <img src={Calcados} alt="Tênis vermelho" />
+                    <p>Calçados</p>
+                </li>
+                <li>
+                    <img src={Variados} alt="Roupa social" />
+                    <p>Variados</p>
+                </li>
+            </ul>
         </div>
     )
 }
