b9590daf0b6912bb6dfbdac32a8de5cc64d8b9a8
luisduccini <rm88360@fiap.com.br>
2021-09-24 10:35:57 -0300

    app

--- lojas-moda/src/App.js
+++ lojas-moda/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import Home from './Componentes/Home/Home.jsx'
 
-
-export function App() {
 export default function App() {
 
   return (
     <div>
       <Home/>
     </div>
-  )
+  );
 }
