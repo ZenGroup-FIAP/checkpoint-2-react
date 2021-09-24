e85719c7356e38d9bcda7fe12d8351d871360259
Matheus Cruz <mccruz2611@gmail.com>
2021-09-24 12:01:57 -0300

    importando fotos e fazendo styled component

--- lojas-moda/src/Componentes/ModaFeminina/ModaFeminina.jsx
+++ lojas-moda/src/Componentes/ModaFeminina/ModaFeminina.jsx
@@ -1,30 +1,40 @@
 import React from 'react'
-import { Titulo, Subtitulo } from '../../styled.js'
+import { Titulo, Subtitulo, SecaoHome, Container, Flexbox, FlexItem} from '../../styled.js'
+import CamisetaFeminina from '../../img/feminino/camisetas-feminino.jpg'
+import CalcasFeminina from '../../img/feminino/calcas-feminino.jpg'
+import CalcadosFeminina from '../../img/feminino/calcados-feminino.jpg'
+import VestidosFeminina from '../../img/feminino/vestidos-feminino.jpg'
 
 export default function ModaFeminina() {
     return (
         <div>
-            <Titulo>Moda feminina</Titulo>
-            <Subtitulo>O melhor e mais sofisticado para homem</Subtitulo>
+            <SecaoHome>
+                <Titulo>Moda feminina</Titulo>
+                <Subtitulo>O melhor e mais sofisticado para homem</Subtitulo>
+            </SecaoHome>    
 
-            <ul>
-                <li>
-                    <img src={null} alt="Camisetas em cabides" />
-                    <p>Camisetas</p>
-                </li>
-                <li>
-                    <img src={null} alt="Calça preta" />
-                    <p>Calças</p>
-                </li>
-                <li>
-                    <img src={null} alt="Tênis vermelho" />
-                    <p>Calçados</p>
-                </li>
-                <li>
-                    <img src={null} alt="Roupa social" />
-                    <p>Social</p>
-                </li>
-            </ul>
+        <Container>    
+            <SecaoHome>
+                <Flexbox>
+                    <FlexItem>
+                        <img src={CamisetaFeminina} alt="Camisetas em cabides" />
+                        <p>Camisetas</p>
+                    </FlexItem>
+                    <FlexItem>
+                        <img src={CalcasFeminina} alt="Calça preta" />
+                        <p>Calças</p>
+                    </FlexItem>
+                    <FlexItem>
+                        <img src={CalcadosFeminina} alt="Tênis vermelho" />
+                        <p>Calçados</p>
+                    </FlexItem>
+                    <FlexItem>
+                        <img src={VestidosFeminina} alt="Roupa social" />
+                        <p>Vestidos</p>
+                    </FlexItem>
+                </Flexbox>
+            </SecaoHome>
+        </Container>
         </div>
     )
 }
