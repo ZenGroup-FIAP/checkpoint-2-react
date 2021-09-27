bb2f97f58a8ecdccc4871d682a27ce3c8e88af84
lucasmatutani <RM87283@fiap.com.br>
2021-09-26 21:38:56 -0300

    Finalizando Rodape.jsx e Bg

--- exercicio5-2/src/components/cabecalho/Cabecalho.jsx
+++ exercicio5-2/src/components/cabecalho/Cabecalho.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
-import {Container, Titulo, CorpoStyled, Subtitulo} from "../../styled.js"
+import {Container, Titulo, CorpoStyled, Subtitulo, BgCabecalho} from "../../styled.js"
 
 export default function Cabecalho() {
     return (
        <div>
-            <Container>
-                <CorpoStyled>
-                    <Titulo>Sorveteria ZenGroup</Titulo>
-                    <Subtitulo>
-                        O melhor lugar para se comprar deliciosos sorvetes.
-                    </Subtitulo>
-                </CorpoStyled>
-            </Container>
+           <BgCabecalho>
+                <Container>
+                    <CorpoStyled>
+                        <Titulo>Sorveteria ZenGroup</Titulo>
+                        <Subtitulo>
+                            O melhor lugar para se comprar deliciosos sorvetes.
+                        </Subtitulo>
+                    </CorpoStyled>
+                </Container>
+            </BgCabecalho>
        </div>
     )
 }
--- exercicio5-2/src/components/rodape/Rodape.jsx
+++ exercicio5-2/src/components/rodape/Rodape.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import { Container, CorpoStyled } from '../../styled'
+import { Container, CorpoStyled, TextoRodape, BgRodape } from '../../styled'
 import Corpo from '../corpo/Corpo'
 
 export default function Rodape() {
     return (
         <div>
-            <Container>
-            <CorpoStyled>
-            <p>Rua ZenCream 707, bairro ZenSpace, cep:00000-000</p>
-            </CorpoStyled>
-            </Container>
+            <BgRodape>
+                <Container>
+                    <CorpoStyled>
+                        <TextoRodape>Rua ZenCream 707, bairro ZenSpace, cep:00000-000</TextoRodape>
+                        <TextoRodape>Sorveteria Zen Group 2021 - Todos os direitos reservados</TextoRodape>
+                    </CorpoStyled>
+                </Container>
+            </BgRodape>
         </div>
     )
 }
